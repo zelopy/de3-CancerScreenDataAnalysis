019b7daeed55d562d38bad55dafa11d8798dcc3a
ryd11 <sdmee97@gmail.com>
2024-05-14 11:44:23 +0900

    total

--- apicrawling.py
+++ apicrawling.py
@@ -1,32 +1,24 @@
-import requests, json, time
-url = 'http://apis.data.go.kr/B551172/CancerScreenorCohortMetaService/getCancerScreenorCohort'
+import requests, json, time, os
+url = 'http://apis.data.go.kr/B551172/CancerScreenorCohortStatusService/getCancerScreenorCohort'
 
 sub = {
-'MaleQuestionMeta',
-'FemaleQuestionMeta',
-'IllnessMeta',
-'TestMeta',
-'AnamnesisMeta',
-'SurgeryMeta',
-'FamilyHistoryMeta',
-'MedicationMeta',
-'FamilyIllnessMeta',
-'DrinkMeta',
-'SmokeMeta',
-'RegiExpirationMeta',
-'MotorMeta',
-'InfectionMeta',
-'GenericMeta',
-'ExamGenericMeta',
-'ExamSpecMeta',
-'ExaminationMeta',
-'HistoryTakingMeta'
+'CancerIncCnt',
+'DeadCnt',
+'AgeSexInfo',
+'BodyMeasmntAvgInfo',
+'GeneralBldTestAvgInfo',
+'TumorMarkerAvgInfo',
+'GeneralChemAvgInfo',
 }
 
 params ={'serviceKey' : 'NGM9g1OzpPvWSeMDkkuWBjDVdLC4fhGD5zPVjvg8nrl/WTJScfQlqMfdHDP3U37ZXo/DCVeXmM5AppVRUjMk3g==', 'pageNo' : '1', 'numOfRows' : '500', 'resultType' : 'json' }
 
 # 최대 재시도 횟수 설정
 max_retries = 5
+
+# 데이터를 저장할 폴더 생성
+if not os.path.exists('data'):
+    os.makedirs('data')
 
 for i in sub:
     url_name = url + i
@@ -37,7 +29,7 @@ for i in sub:
     while retries < max_retries:
         try:
             response = requests.get(url_name, params=params)
-            with open(i + '.json', 'w', encoding='utf-8') as f:
+            with open(os.path.join('data', i + '.json'), 'w', encoding='utf-8') as f:
                 json.dump(response.json(), f, ensure_ascii=False, indent=4)
             # 성공했으므로 루프를 빠져나옴
             break
